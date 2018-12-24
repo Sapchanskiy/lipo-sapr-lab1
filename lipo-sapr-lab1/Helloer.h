096047584a2c5210073a02fa14b76b5c1337b6b2
Сергей <sewer007@mail.ru>
2018-12-25 01:18:45 +0300

    Добавлена лабораторная работа №1

---
+++ lipo-sapr-lab1/Helloer.h
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef helloer_h
+#define helloer_h
+#include <iostream>
+
+using namespace std;
+
+class Helloer {
+public:
+	Helloer();
+	Helloer(const string& aMessage);
+	std::string printMessage();
+private:
+	std::string message;
+};
+
+#endif
